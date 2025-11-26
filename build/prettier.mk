c2675f5985bac21644f10305915ef505e4bc1622
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-11-26 13:15:50 -0800

    feat(format): add prettier config for consistent formatting
    Signed-off-by: Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>

--- build/prettier.mk
+++ build/prettier.mk
@@ -4,7 +4,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#    
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -16,16 +16,18 @@
 
 ##@ Formatting (of JSON files)
 
-PRETTIER_VERSION := 3.3.3
+PRETTIER_VERSION := 3.6.2
 
 .PHONY: format-check
 format-check: ## Checks the formatting of JSON files.
 	@echo "$(ARROW) Checking for formatting issues using prettier..."
 	@echo ""
-	@npx --yes prettier@$(PRETTIER_VERSION) --check "*/**/*.{ts,js,mjs,json}"
+	@npm install --no-save prettier-plugin-multiline-arrays
+	@npx --yes prettier@$(PRETTIER_VERSION) --config ./.github/linters/.prettierrc.yml --check "*/**/*.{ts,tsx,js,jsx,js,cjs,mjs,json,jsonc}"
 
 .PHONY: format-write
 format-write: ## Updates the formatting of JSON files.
 	@echo "$(ARROW) Reformatting files using prettier..."
 	@echo ""
-	@npx --yes prettier@$(PRETTIER_VERSION) --write "*/**/*.{ts,js,mjs,json}"
+	@npm install --no-save prettier-plugin-multiline-arrays
+	@npx --yes prettier@$(PRETTIER_VERSION) --config ./.github/linters/.prettierrc.yml --write "*/**/*.{ts,tsx,js,jsx,js,cjs,mjs,json,jsonc}"
